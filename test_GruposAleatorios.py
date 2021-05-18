f56be6733313adece484deeb28c77f4f3fb4a0c1
Osv04 <oscar.veloz4@gmail.com>
2021-05-17 21:32:57 -0400

    Add 2 python test

--- test_GruposAleatorios.py
+++ test_GruposAleatorios.py
@@ -1,1 +1,24 @@
 import pytest
+
+
+#Prueba de Division
+@pytest.mark.parametrize(
+    "lst, div, except",
+    [
+        (["Lenore", "Candice", "Chanel", "Chet","Lorenzo"],5,1), 
+        (["Lenore", "Candice", "Chanel", "Lorenzo"],4,2), 
+        (["Lenore", "Candice", "Chanel", "Chet","Lorenzo","Juan","Maria","Jaime","Pedro","David","Jesus","Moises","Herodes"],6,2), 
+        (["Lenore", "Candice", "Chanel", "Chet","Lorenzo","Juan","Maria","Jaime","Pedro","David","Jesus","Moises","Herodes"],5,2), 
+    ]
+)
+
+#Prueba de Remanente
+@pytest.mark.parametrize(
+    "lst, rem, except",
+    [
+        (["Lenore", "Candice", "Chanel", "Chet","Lorenzo"],5,0), 
+        (["Lenore", "Candice", "Chanel", "Lorenzo"],4,0), 
+        (["Lenore", "Candice", "Chanel", "Chet","Lorenzo","Juan","Maria","Jaime","Pedro","David","Jesus","Moises","Herodes"],6,1), 
+        (["Lenore", "Candice", "Chanel", "Chet","Lorenzo","Juan","Maria","Jaime","Pedro","David","Jesus","Moises","Herodes"],5,3), 
+    ]
+)
